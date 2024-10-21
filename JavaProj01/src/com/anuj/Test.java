cb13ed702d7fda627e028a41a58c015769664ba4
anuj <vanuj7172@gmail.com>
2024-10-21 12:31:09 +0530

    initial commit

---
+++ JavaProj01/src/com/anuj/Test.java
@@ -0,0 +1,15 @@
+package com.anuj;
+
+public class Test {
+	
+	public static void  m1() {
+		
+		System.out.println("Test.m1()");
+	}
+
+	public static void main(String[] args) {
+		m1();
+
+	}
+
+}
