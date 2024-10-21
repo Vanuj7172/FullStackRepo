317b5e9013a0682b692ef68056ac107f512488c1
anuj <vanuj7172@gmail.com>
2024-10-21 12:46:36 +0530

    changes by dev

--- JavaProj01/src/com/anuj/Test.java
+++ JavaProj01/src/com/anuj/Test.java
@@ -6,6 +6,12 @@ public class Test {
 		
 		System.out.println("Test.m1()");
 	}
+	
+	public static void  m2() {
+		
+		System.out.println("dev-m2-method");
+	}
+	
 
 	public static void main(String[] args) {
 		m1();
